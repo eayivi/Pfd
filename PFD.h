a094830ce8c3d4d7f5f11b6c3c140594d2db197d
eayivi <enam.ayivi@utexas.edu>
2013-06-27 18:42:57 -0500

    completed comments for PFD.h

--- PFD.h
+++ PFD.h
@@ -3,10 +3,14 @@
  * Sophia Hernandez smh3665
  * Enam Ayivi 
  */
+
  
 #ifndef PFD_h
 #define PFD_h
 
+// --------
+// includes
+// --------
 
 #include <iostream>
 #include <vector>
@@ -26,32 +30,50 @@ struct Vertex{
 
 };
 
+// -------------
+//	pfd_solve
+// -------------
+
+
 /**
- * pdf_solve
- * @param r, w std:: istream and ostream respectively
+ * read, eval, print values
+ * @param w a std::ostream
+ * @param r a std::istream
  *
  */
 void pfd_solve(std::istream&, std::ostream&);
 
 
+// ------------
+// pfd_read
+// ------------
 /**
- * pdf_read
- * @param 
- * return std::vector<std::vector<int> >
+ * reads two ints into i and j
+ * @param r a std::istream
+ * @param i an int by reference
+ * @param j an int by reference
+ * @return std::vector<std::vector<int> >
  */
 std::vector<Vertex> pfd_read (std::istream&, int&, int&);
 
+
+// ------------
+// pfd_eval
+// ------------
 /**
- * pdf_eval
- * @param 
- *
+ * 
+ * @param v a vector of struct Vertex
+ * @return a set of int that in order of required execution
  */
 std::vector<int> pfd_eval (std::vector<Vertex>&);
 
+// -------------
+// pfd_print
+// -------------
 /**
- * pdf_print
- * @param 
- *
+ * print the value to the stream
+ * @param w a std::ostream
+ * @ param v a vector of int with the values to print
  */
  void pfd_print(std::ostream&, const std::vector<int>&);
 
